88f67ac5ef74ec9b9d1c2ccf293466e081fd885e
Theblessed <>
2023-03-07 16:49:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function olamundo() {    
+  alert('Olá Mundo');}
+function calcularconsumo() 
+{    var km, gas, consumo    
+  km = document.getElementById('km').value    
+    gas = document.getElementById('gas').value    
+      consumo = km / gas    
+        alert(consumo)}
